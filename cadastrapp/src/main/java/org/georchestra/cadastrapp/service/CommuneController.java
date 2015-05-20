4496fc160443858c5f7df770f3384a2794d7a155
pierrejego <pierre.jego@gfi.fr>
2015-05-20 12:06:05 +0200

    Create first service getCommune

---
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CommuneController.java
@@ -0,0 +1,45 @@
+package org.georchestra.cadastrapp.service;
+
+
+import javax.annotation.Resource;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.georchestra.cadastrapp.JsonBean;
+import org.georchestra.cadastrapp.model.Commune;
+
+
+@Path("/getCommune")
+public class CommuneController {
+	
+	@Resource(name="dbDataSource")
+	private DataSource dataSource;
+
+    @GET
+    @Path("/id/{input}")
+    @Produces("application/json")
+    public List<Map<String,Object>> getCommuneList(@PathParam("input") String input) throws SQLException {
+    	
+	
+    	String query = "select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune";
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        List<Map<String,Object>> communes = jdbcTemplate.queryForList(query);
+            
+              
+        return communes;
+    }
+
+}
+
