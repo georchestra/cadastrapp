1d2f3fc2943ff2cc11779ab086cee684d7d6e7d2
pierrejego <pierre.jego@gfi.fr>
2015-06-01 16:22:53 +0200

    Change QueryPath to QueryParam

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CommuneController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CommuneController.java
@@ -3,15 +3,14 @@ package org.georchestra.cadastrapp.service;
 
 import javax.annotation.Resource;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -31,7 +30,7 @@ public class CommuneController {
     @GET
     @Path("/all")
     @Produces("application/json")
-    public List<Map<String,Object>> getCommunesList() throws SQLException {
+    public List<Map<String,Object>> getCommunesList(){
     	
 	
     	String query = "select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune where ccocom is not null;";
@@ -43,11 +42,30 @@ public class CommuneController {
     }
     
     @GET
-    @Path("/libcom_partiel/{input}")
     @Produces("application/json")
-    public List<Map<String,Object>> getCommunesListByLibcom(@PathParam("input") String libCom) throws SQLException {
+    /**
+     *  GetCommune returns list of communes depending user group authorization
+     *  
+     * @param libcom_partiel
+     * @param ccoinsee_partiel
+     * @return JSON export of commune view
+     * 
+     * @throws SQLException
+     */
+    public List<Map<String,Object>> getCommunesListByLibcom(
+    			@QueryParam("libcom_partiel") String libCom,
+    			@QueryParam("ccoinsee_partiel") String ccoinsee){
     	
-    	 List<Map<String,Object>> communes = null;
+    	List<Map<String,Object>> communes = null;
+    	boolean clauseExists = false;
+    	
+    	StringBuilder queryBuilder = new StringBuilder();
+    	queryBuilder.append("select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune");
+    	
+    	// If one of the parameter is present only one clause
+    	 if((libCom == null || libCom.isEmpty()) && (ccoinsee == null || ccoinsee.isEmpty())){
+    		 logger.warn("No parameter in request");
+    	 }
     	
     	 // Check if libcom is not null
     	 // TODO See if we limit search with n characters
@@ -57,30 +75,14 @@ public class CommuneController {
     		String newLibCom = StringUtils.stripAccents(libCom);
 	    		    	 
     		// Create and execute request
-	    	String query = "select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune where libcom LIKE '" + newLibCom.toUpperCase() +"%' and ccocom is not null; ;";
-	        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-	        communes = jdbcTemplate.queryForList(query);
-            
+    		queryBuilder.append(" where libcom LIKE '")
+    			.append(newLibCom.toUpperCase())
+    			.append("%'"); 
+    		
+    		clauseExists = true;            
     	}
-    	//TODO add exception management
-    	else{
-    		//log empty request
-    		logger.info("Null or Empty libcom in request");
-    	}
-         
-    	// Return value providers will convert to JSON
-        return communes;
-    }
-    
-    @GET
-    @Path("/ccoinsee_partiel/{input}")
-    @Produces("application/json")
-    public List<Map<String,Object>> getCommunesListByCCOINSEE(@PathParam("input") String ccoinsee) throws SQLException {
     	
-    	 List<Map<String,Object>> communes = null;
-    	
-    	 // Check if libcom is not null
-    	 // TODO See if we limit search with n characters
+    	// Check if ccoinsee is nont null
     	if(ccoinsee != null && !ccoinsee.isEmpty()){
     		
     		// Special case when code commune on 5 characters is given
@@ -88,59 +90,26 @@ public class CommuneController {
     		if(5 == ccoinsee.length()){
     			ccoinsee = ccoinsee.substring(0, 2) + "%" +ccoinsee.substring(2);    			
     		}
-	    	    		    		    	 
-    		// Create and execute request
-	    	String query = "select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune where ccoinsee LIKE '%" + ccoinsee +"%' and ccocom is not null;;";
-	        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-	        communes = jdbcTemplate.queryForList(query);
-            
-    	}else{
-    		//log empty request
-    		logger.info("Null or Empty ccoinsee in request");
-    	}
-         
-    	// Return value providers will convert to JSON
-        return communes;
-    }
-    
-    
-    @GET
-    @Path("libcom_partiel/{libcom}/ccoinsee_partiel/{ccoinsee}")
-    @Produces("application/json")
-    public List<Map<String,Object>> getCommunesListByCCOINSEE(@PathParam("libcom") String libCom, @PathParam("ccoinsee") String ccoinsee) throws SQLException {
-    	
-    	 List<Map<String,Object>> communes = null;
-    	
-    	 // If no libcomm call CCOInsee
-    	 if(libCom == null || libCom.isEmpty()){
-    		 communes = getCommunesListByCCOINSEE(ccoinsee);
-    	 }
-    	 // If non ccoinse call libcom
-    	 else if(ccoinsee == null || ccoinsee.isEmpty()){
-    		 communes = getCommunesListByLibcom(libCom);
-    	}
-    	else{
-    	
-    		// Special case when code commune on 5 characters is given
-    		// Convert 350206 to 35%206 for query
-    		if(5 == ccoinsee.length()){
-    			ccoinsee = ccoinsee.substring(0, 2) + "%" +ccoinsee.substring(2);    			
+	    	
+    		if (clauseExists){
+    			queryBuilder.append(" and ");
     		}
-    		// request in without accents
-    		String newLibCom = StringUtils.stripAccents(libCom);
-	    	    		    		    	 
+    		else{
+    			queryBuilder.append(" where ");
+    		}
     		// Create and execute request
-	    	String query = "select ccoinsee, libcom, libcom_min from cadastreapp_qgis.commune where ccoinsee LIKE '%" + ccoinsee +"%' and libcom LIKE '" + newLibCom.toUpperCase() +"%' ;";
-	        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-	        communes = jdbcTemplate.queryForList(query);
-            
+    		queryBuilder.append(" ccoinsee LIKE '%")
+    			.append(ccoinsee)
+    			.append("%'");
+    		
     	}
-         // TODO exception management
-    	 
+    	queryBuilder.append(" and ccocom is not null;"); 
+         
+    	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        communes = jdbcTemplate.queryForList(queryBuilder.toString());
+        
     	// Return value providers will convert to JSON
         return communes;
     }
-
-
+    
 }
-
