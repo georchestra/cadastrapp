3d40907cb8bc7a7bc89134a768e1922d34884f25
ntasia <nicolas.tasia@gfi.fr>
2015-06-05 09:24:03 +0200

    ExtFormResult

---
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/model/ExtFormResult.java
@@ -0,0 +1,24 @@
+package org.georchestra.cadastrapp.model;
+
+public class ExtFormResult {
+
+	private boolean success = false;
+	
+	private Object data = null;
+	
+
+	public ExtFormResult(boolean success, Object data) {
+		this.success = success;
+		this.data = data;
+	}
+
+
+	public boolean getSuccess() {
+		return success;
+	}
+
+
+	public Object getData() {
+		return data;
+	}
+}
