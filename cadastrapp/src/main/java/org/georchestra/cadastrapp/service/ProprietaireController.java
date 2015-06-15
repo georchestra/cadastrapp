ee22af7d089c27a8fcc570d075526118a2162c51
pierrejego <pierre.jego@gfi.fr>
2015-06-15 19:36:12 +0200

    Change input parameters for owners

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/ProprietaireController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/ProprietaireController.java
@@ -1,32 +1,17 @@
 package org.georchestra.cadastrapp.service;
 
 
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.StringReader;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.ObjectWriter;
-import org.georchestra.cadastrapp.model.ExtFormResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -39,21 +24,28 @@ public class ProprietaireController extends CadController{
 
     @GET
     @Produces("application/json")
-    public List<Map<String,Object>> getProprietairesList(@Context HttpHeaders headers,@QueryParam("dnomlp") String dnomlp) throws SQLException {
+    public List<Map<String,Object>> getProprietairesList(
+    			@Context HttpHeaders headers,
+    			@QueryParam("dnomlp_partiel") String dnomlpPartiel,
+    			@QueryParam("ccoinsee") String ccoinsee,
+    			@QueryParam("dnupro") String dnupro
+    			) throws SQLException {
     	
     	List<Map<String,Object>> proprietaires = null;
     	
     	if (getUserCNILLevel(headers)>0){
-    	    	
-	    	if(dnomlp != null && !dnomlp.isEmpty() && 3<dnomlp.length()){
-	    		//TODO change request with dpmlp
-		    	//String query = "select dnomlp, dpmlp, dprnlp, expnee, dnomcp, dprncp, dlign3, dlign4, dlign5, dlign6, dldnss, jdatnss, ccodro_lib from cadastreapp_qgis.proprietaire where dnomlp LIKE '%"+dnomlp+"%';";
-		       StringBuilder queryBuilder = new StringBuilder();
+    	    
+    		// No search if all parameters are null or dnomlpPariel less than 3 char
+	    	if((dnomlpPartiel != null && !dnomlpPartiel.isEmpty() && 3<dnomlpPartiel.length()) 
+	    			||  ccoinsee!=null || dnupro!=null){
+	    		StringBuilder queryBuilder = new StringBuilder();
 		       queryBuilder.append("select dnomlp, dprnlp, epxnee, dnomcp, dprncp, dlign3, dlign4, dlign5, dlign6, dldnss, jdatnss, ccodro_lib");
 		       queryBuilder.append(" from ");
 		       queryBuilder.append(databaseSchema);
 		       queryBuilder.append(".proprietaire");
-		       queryBuilder.append(createLikeClauseRequest("dnomlp", dnomlp));
+		       queryBuilder.append(createLikeClauseRequest("dnomlp", dnomlpPartiel));
+		       queryBuilder.append(createEqualsClauseRequest("ccoinsee", ccoinsee));
+		       queryBuilder.append(createEqualsClauseRequest("dnupro", dnupro));
 		       queryBuilder.append(finalizeQuery());
 	 	       
 		    	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
