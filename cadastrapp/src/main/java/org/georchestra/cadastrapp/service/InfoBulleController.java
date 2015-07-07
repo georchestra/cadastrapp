b3d311abb9e4d8a644c2bf6f9674d5ec9006f0ec
pierrejego <pierre.jego@gfi.fr>
2015-07-07 18:01:19 +0200

    Separate infobulle service in three services Parcelle and Unite fonciere

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/InfoBulleController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/InfoBulleController.java
@@ -16,12 +16,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@Path("/getInfoBulle")
+
 public class InfoBulleController extends CadController {
 	
 	final static Logger logger = LoggerFactory.getLogger(InfoBulleController.class);
 
 
+	@Path("/getInfoBulle")
 	@GET
 	@Produces("application/json")
 	/**
@@ -43,6 +44,80 @@ public class InfoBulleController extends CadController {
 			@DefaultValue("1") @QueryParam("infouf") String infouf) throws SQLException {
  
 		List<Map<String, Object>> informations = null;
+		
+		if(infocadastrale.equals("0") && infouf.equals("1")){
+			informations = getInfoBulleUniteFonciere(headers, parcelle);
+		}else if (infocadastrale.equals("1") && infouf.equals("0")){
+			informations = getInfoBulleParcelle(headers, parcelle);
+		}else if (infocadastrale.equals("0") && infouf.equals("0")){
+			logger.warn("No information can be serve");
+		}else if (isMandatoryParameterValid(parcelle)){
+		
+			// Create query
+			StringBuilder queryBuilder = new StringBuilder();
+	
+			queryBuilder.append("select ");
+	
+			
+			// TODO Add surfacecalculee
+			queryBuilder.append("p.parcelle, c.libcom, p.dcntpa, p.dnvoiri, p.dindic, p.cconvo, p.dvoilib");
+	
+			if(getUserCNILLevel(headers)>0){
+				//TODO add proprietaires
+				queryBuilder.append(", prop.ddenom");
+			}
+		
+			//TODO check dcnptap_sum, sigcal_sum, batical
+			queryBuilder.append(", p.comptecommunal");
+
+			queryBuilder.append(" from ");
+			queryBuilder.append(databaseSchema);
+			queryBuilder.append(".parcelle p,");
+			queryBuilder.append(databaseSchema);
+			queryBuilder.append(".commune c ");
+			
+			if(getUserCNILLevel(headers)>0){
+				queryBuilder.append(",");			
+				queryBuilder.append(databaseSchema);
+				queryBuilder.append(".proprietaire prop ");
+			}
+				
+			queryBuilder.append(createEqualsClauseRequest("p.parcelle", parcelle));
+			queryBuilder.append(" and p.ccocom = c.ccocom and p.ccodep = c.ccodep");
+			
+			if(getUserCNILLevel(headers)>0){
+				queryBuilder.append(" and p.dnupro = prop.dnupro");
+			}
+			queryBuilder.append(finalizeQuery());
+						
+			JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+			informations = jdbcTemplate.queryForList(queryBuilder.toString());
+		}
+		else{
+			logger.warn("missing mandatory parameters");
+		}
+
+		return informations;
+	}
+
+	
+	@Path("/getInfobulleParcelle")
+	@GET
+	@Produces("application/json")
+	/**
+	 * 
+	 * @param headers / headers from request used to filter search using LDAP Roles to display only information about parcelle from available ccoinsee
+	 * 
+	 * @param parcelle id parcelle
+	 * @return Data from parcelle view to be display in popup in JSON format
+	 * 
+	 * @throws SQLException
+	 */
+	public List<Map<String, Object>> getInfoBulleParcelle(
+			@Context HttpHeaders headers,
+			@QueryParam("parcelle") String parcelle) throws SQLException {
+ 
+		List<Map<String, Object>> informations = null;
 
 		if (isMandatoryParameterValid(parcelle)){
 		
@@ -51,26 +126,31 @@ public class InfoBulleController extends CadController {
 	
 			queryBuilder.append("select ");
 	
-			// TODO Add libcom and surfacecalculee
-			// queryBuilder.append("parcelle, libcom, dcntpa, surfacecalculee");
-			queryBuilder.append("p.parcelle, c.libcom, p.dcntpa");
-	
+			// TODO Add surfacecalculee
+			queryBuilder.append("p.parcelle, c.libcom, p.dcntpa, p.dnvoiri, p.dindic, p.cconvo, p.dvoilib");
+			
 			if(getUserCNILLevel(headers)>0){
 				//TODO add proprietaires
-				queryBuilder.append(", p.dnupro");
+				queryBuilder.append(", prop.ddenom");
 			}
-			if(infouf.equals("1")){
-				//TODO check dcnptap_sum, sigcal_sum, batical
-				queryBuilder.append(", p.comptecommunal, p.dcntpa");
-			}
+			
 			queryBuilder.append(" from ");
 			queryBuilder.append(databaseSchema);
 			queryBuilder.append(".parcelle p,");
 			queryBuilder.append(databaseSchema);
 			queryBuilder.append(".commune c ");
-		
+			
+			if(getUserCNILLevel(headers)>0){
+				queryBuilder.append(",");			
+				queryBuilder.append(databaseSchema);
+				queryBuilder.append(".proprietaire prop ");
+			}
 			queryBuilder.append(createEqualsClauseRequest("p.parcelle", parcelle));
 			queryBuilder.append(" and p.ccocom = c.ccocom and p.ccodep = c.ccodep");
+			
+			if(getUserCNILLevel(headers)>0){
+				queryBuilder.append(" and p.dnupro = prop.dnupro");
+			}
 			
 			//TODO add ccodep 
 			//queryBuilder.append(" and p.ccodep = c.ccodep");
@@ -86,5 +166,50 @@ public class InfoBulleController extends CadController {
 		return informations;
 	}
 
+	@Path("/getInfobulleUniteFonciere")
+	@GET
+	@Produces("application/json")
+	/**
+	 * 
+	 * @param headers / headers from request used to filter search using LDAP Roles to display only information about parcelle from available ccoinsee
+	 * 
+	 * @param parcelle id parcelle
+	 * @return Data from parcelle view to be display in popup in JSON format
+	 * 
+	 * @throws SQLException
+	 */
+	public List<Map<String, Object>> getInfoBulleUniteFonciere(
+			@Context HttpHeaders headers,
+			@QueryParam("parcelle") String parcelle) throws SQLException {
+ 
+		List<Map<String, Object>> informations = null;
+
+		if (isMandatoryParameterValid(parcelle)){
+		
+			// Create query
+			StringBuilder queryBuilder = new StringBuilder();
+	
+			queryBuilder.append("select ");
+				
+			//TODO check dcnptap_sum, sigcal_sum, batical
+			queryBuilder.append(" p.comptecommunal, p.dcntpa");
+			
+			queryBuilder.append(" from ");
+			queryBuilder.append(databaseSchema);
+			queryBuilder.append(".parcelle p");
+		
+			queryBuilder.append(createEqualsClauseRequest("p.parcelle", parcelle));
+
+			queryBuilder.append(finalizeQuery());
+						
+			JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+			informations = jdbcTemplate.queryForList(queryBuilder.toString());
+		}
+		else{
+			logger.warn("missing mandatory parameters");
+		}
+
+		return informations;
+	}
 
 }
