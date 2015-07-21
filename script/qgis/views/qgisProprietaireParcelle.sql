af8745e80b6e13cb683ed373cae8ea7636e84777
pierrejego <pierre.jego@gfi.fr>
2015-07-21 10:27:19 +0200

    Correction of case where parcelle does not have lot

--- script/qgis/views/qgisProprietaireParcelle.sql
+++ script/qgis/views/qgisProprietaireParcelle.sql
@@ -5,9 +5,13 @@
 create view #schema_cadastrapp.proprietaire_parcelle as 
 select *
   FROM dblink('host=#DBHost_qgis dbname=#DBName_qgis user=#DBUser_qgis password=#DBpasswd_qgis'::text,
-	'select  lo.lots,lo.parcelle,lo.comptecommunal,l.dnupro,lo.dnulot from lots lo
-		left join local10 l on l.comptecommunal=lo.comptecommunal
-		order by lots,dnulot,parcelle asc'::text)
+  'select  lo.lots,lo.parcelle,lo.comptecommunal,l.dnupro,lo.dnulot from lots lo
+	left join local10 l on l.comptecommunal=lo.comptecommunal
+	UNION
+	select distinct ''0'' as lots,p.parcelle,p.comptecommunal,po.dnupro,''0'' as dnulot
+	from parcelle p
+	left join proprietaire po on p.comptecommunal=po.comptecommunal
+	where p.gpdl=''0'' '::text) 
 proprietaire_parcelle (
 	lots character varying(29), 
 	parcelle  character varying(19), 
