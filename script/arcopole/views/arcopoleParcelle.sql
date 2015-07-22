e74f85519b4b9eb6e975230394af560ae629e020
pierrejego <pierre.jego@gfi.fr>
2015-07-22 09:56:08 +0200

    Correction of missing surfc in parcelle view

--- script/arcopole/views/arcopoleParcelle.sql
+++ script/arcopole/views/arcopoleParcelle.sql
@@ -34,7 +34,7 @@ ALTER TABLE #schema_cadastrapp.parcelle OWNER TO #user_cadastrapp;
 -- DROP VIEW #schema_cadastrapp.parcelledetails;
 
 CREATE OR REPLACE VIEW #schema_cadastrapp.parcelledetails AS
- SELECT dgi_nbati.parcelle, dgi_nbati.lot, dgi_nbati.ccocom, dgi_nbati.dnupla, dgi_nbati.dcntpa, dgi_nbati.dsrpar, dgi_nbati.dnupro, dgi_nbati.jdatat, dgi_nbati.dreflf, dgi_nbati.gpdl, dgi_nbati.cprsecr, dgi_nbati.ccosecr, dgi_nbati.dnuplar, dgi_nbati.dnupdl, dgi_nbati.gurbpa, dgi_nbati.dparpi, dgi_nbati.ccoarp, dgi_nbati.gparnf, dgi_nbati.gparbat, dgi_nbati.dnvoiri, dgi_nbati.dindic, dgi_nbati.ccovoi, dgi_nbati.ccoriv, dgi_nbati.ccocif, dgi_nbati.cconvo, dgi_nbati.dvoilib, dgi_nbati.ccocomm, dgi_nbati.ccoprem, dgi_nbati.ccosecm, dgi_nbati.dnuplam, dgi_nbati.type_filiation, dgi_nbati.annee, dgi_nbati.ccodep, dgi_nbati.ccodir, dgi_nbati.ccopre, dgi_nbati.ccosec, dgi_nbati.comptecommunal, dgi_nbati.pdl, dgi_nbati.inspireid
+ SELECT dgi_nbati.parcelle, dgi_nbati.lot, dgi_nbati.ccocom, dgi_nbati.dnupla, dgi_nbati.dcntpa, dgi_nbati.dsrpar, dgi_nbati.dnupro, dgi_nbati.jdatat, dgi_nbati.dreflf, dgi_nbati.gpdl, dgi_nbati.cprsecr, dgi_nbati.ccosecr, dgi_nbati.dnuplar, dgi_nbati.dnupdl, dgi_nbati.gurbpa, dgi_nbati.dparpi, dgi_nbati.ccoarp, dgi_nbati.gparnf, dgi_nbati.gparbat, dgi_nbati.dnvoiri, dgi_nbati.dindic, dgi_nbati.ccovoi, dgi_nbati.ccoriv, dgi_nbati.ccocif, dgi_nbati.cconvo, dgi_nbati.dvoilib, dgi_nbati.ccocomm, dgi_nbati.ccoprem, dgi_nbati.ccosecm, dgi_nbati.dnuplam, dgi_nbati.type_filiation, dgi_nbati.annee, dgi_nbati.ccodep, dgi_nbati.ccodir, dgi_nbati.ccopre, dgi_nbati.ccosec, dgi_nbati.comptecommunal, dgi_nbati.pdl, dgi_nbati.inspireid, surfc
    FROM dblink('host=#DBHost_arcopole dbname=#DBName_arcopole user=#DBUser_arcopole password=#DBpasswd_arcopole'::text, '
    select
    codparc as parcelle,
@@ -74,12 +74,16 @@ CREATE OR REPLACE VIEW #schema_cadastrapp.parcelledetails AS
     			substr(codparc,7,3) as ccopre,
    				substr(codparc,10,2) ccosec ,
                 dnupro as comptecommunal,
-                ''pdl'' as pdl, ''FR''||codparc as inspireId from #DBSchema_arcopole.dgi_nbati'::text) dgi_nbati(parcelle character varying(19), lot character varying, ccocom character varying(3),
+                ''pdl'' as pdl, ''FR''||codparc as inspireId
+                FROM #DBSchema_arcopole.dgi_nbati'::text) dgi_nbati(parcelle character varying(19), lot character varying, ccocom character varying(3),
                 dnupla character varying(4), dcntpa numeric(38,8), dsrpar character varying(1), dnupro character varying(12), jdatat character varying(8),
                 dreflf character varying(5), gpdl character varying(1), cprsecr character varying(3), ccosecr character varying(2), dnuplar character varying(4),
                 dnupdl character varying(3), gurbpa character varying(1), dparpi character varying(4), ccoarp character varying(1), gparnf character varying(1),
                 gparbat character varying(1), dnvoiri character varying(4), dindic character varying(1), ccovoi character varying(5), ccoriv character varying(4),
                 ccocif character varying(4), cconvo character varying(4), dvoilib character varying(26), ccocomm character varying(3), ccoprem character varying(3),
-                ccosecm character varying(2), dnuplam character varying(4), type_filiation character varying(1), annee character varying(4), ccodep character varying(2), ccodir character varying(1), ccopre character varying(3), ccosec character varying(2), comptecommunal character varying(15), pdl character varying(22), inspireid character varying(17));
+                ccosecm character varying(2), dnuplam character varying(4), type_filiation character varying(1), annee character varying(4), ccodep character varying(2), 
+                ccodir character varying(1), ccopre character varying(3), ccosec character varying(2), 
+                comptecommunal character varying(15), pdl character varying(22), inspireid character varying(17))
+                left join #schema_cadastrapp.v_parcelle_surfc p2 on parcelle.parcelle=p2.parcelle;
                 
 ALTER TABLE #schema_cadastrapp.parcelledetails OWNER TO #user_cadastrapp;
--- script/arcopole/views/arcopoleParcelleSurface.sql
+++ script/arcopole/views/arcopoleParcelleSurface.sql
@@ -1,7 +1,11 @@
+-- View: cadastrapp_arcopole.parcelle
+
+-- DROP VIEW #schema_cadastrapp.v_parcelle_surfc;
+
 create view  #schema_cadastrapp.v_parcelle_surfc as select  * 
 	FROM dblink('host=#DBHost_arcopole dbname=#DBName_arcopole user=#DBUser_arcopole password=#DBpasswd_arcopole'::text, 
-			'select distinct id_parc as #DBSchema_arcopole.parcelle,st_area(shape) as surfc from edi_parc'::text)
-	parcelle_surfc(parcelle character varying(19),surfc float);
+		'select distinct id_parc as parcelle,st_area(shape) as surfc from #DBSchema_arcopole.edi_parc'::text)
+			parcelle_surfc(parcelle character varying(19),surfc float);
 	
 ALTER TABLE #schema_cadastrapp.v_parcelle_surfc OWNER TO #user_cadastrapp;
 
