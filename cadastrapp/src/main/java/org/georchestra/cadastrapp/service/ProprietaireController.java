a733ae63db3568cb3df89c0cfbb2609d9e4ea21e
pierrejego <pierre.jego@gfi.fr>
2015-07-23 18:11:51 +0200

    Add specific details level to get name or id only

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/ProprietaireController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/ProprietaireController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -29,37 +30,58 @@ public class ProprietaireController extends CadController{
      * This will return information about owners in JSON format
      * 
      * @param headers headers from request used to filter search using LDAP Roles
-     * @param dnomlpPartiel partial name to be search, must be have at least 3 char
+     * @param dnomlp partial name to be search, must be have at least 3 char
      * @param ccoinsee  code commune like 630103 (codep + codir + cocom)
      * 					ccoinsee should be on 6 char, if only 5 we deduce that codir is not present
      * @param dnupro id to be search, a same dnupro can be found in several commune
+     * @param comptecommunal id specific for a owner
+     * @param details -> change list of fields in result 0 dy default or if not present
+     * 					0 : dnomlp, dprnlp
+     * 					1 : dnomlp, dprnlp, epxnee, dnomcp, dprncp, dlign3, dlign4, dlign5, dlign6, dldnss, jdatnss, ccodro_lib, comptecommunal will be return
+     * 					2 : comptecommunal
+     * 					 
      * 
-     * @return 
+     * @return list of information about proprietaire depending on details level asked
+     * 
      * @throws SQLException
      */
     public List<Map<String,Object>> getProprietairesList(
     			@Context HttpHeaders headers,
-    			@QueryParam("dnomlp_partiel") String dnomlpPartiel,
+    			@QueryParam("dnomlp") String dnomlp,
     			@QueryParam("ccoinsee") String ccoinsee,
-    			@QueryParam("dnupro") String dnupro
+    			@QueryParam("dnupro") String dnupro,
+    			@QueryParam("compteCommunal") String compteCommunal,
+    			@DefaultValue("0") @QueryParam("details") int details
     			) throws SQLException {
     	
-    	List<Map<String,Object>> proprietaires = null;
+    	// Init list to return response even if nothing in it.
+    	List<Map<String,Object>> proprietaires = new ArrayList<Map<String,Object>>();;
     	List<String> queryParams = new ArrayList<String>();
     	
+    	logger.info("details : " + details);
+    	// User need to be at least CNIL1 level
     	if (getUserCNILLevel(headers)>0){
     	    
     		// No search if all parameters are null or dnomlpPariel less than 3 char
-	    	if((dnomlpPartiel != null && !dnomlpPartiel.isEmpty() && 3<dnomlpPartiel.length()) 
-	    			||  ccoinsee!=null || dnupro!=null){
+	    	if((dnomlp != null && !dnomlp.isEmpty() && 2<dnomlp.length()) 
+	    			&& (ccoinsee!=null || dnupro!=null)){
 	    		
 	    		StringBuilder queryBuilder = new StringBuilder();
-		       queryBuilder.append("select dnomlp, dprnlp, epxnee, dnomcp, dprncp, dlign3, dlign4, dlign5, dlign6, dldnss, jdatnss, ccodro_lib");
-		       queryBuilder.append(" from ");
-		       queryBuilder.append(databaseSchema);
-		       queryBuilder.append(".proprietaire");
-		      
-		       queryBuilder.append(createLikeClauseRequest("dnomlp", dnomlpPartiel, queryParams));
+	    		
+	    		logger.info("details : " + details);
+	    		
+	    		if(details == 2){
+	    			queryBuilder.append("select distinct comptecommunal ");   		    	   			    
+	    		}
+	    		else if(details == 1){
+	    			queryBuilder.append("select RTRIM(dnomlp) as dnomlp, RTRIM(dprnlp) as dprnlp, epxnee, dnomcp, RTRIM(dprncp) as dprncp, dlign3, dlign4, dlign5, dlign6, dldnss, jdatnss, ccodro_lib, comptecommunal ");   			    
+	    		}
+	    		else{
+	    			queryBuilder.append("select distinct RTRIM(dnomlp) as dnomlp, RTRIM(dprnlp) as dprnlp ");				    		    		
+	    		}
+		        queryBuilder.append(" from ");
+		        queryBuilder.append(databaseSchema);
+		        queryBuilder.append(".proprietaire");
 		      
 		       //TODO factorize this
 		       // no ccoinsee present in view proprietaire, parse it to get ccodep, ccocom and ccodir
@@ -79,9 +101,15 @@ public class ProprietaireController extends CadController{
 		    		   String ccodir = ccoinsee.substring(2, 3);
 		    		   queryBuilder.append(createEqualsClauseRequest("ccodir", ccodir, queryParams));
 		    	   }  
-		       }
-		      
+		       }	      
+		       queryBuilder.append(" and UPPER(dnomlp) LIKE UPPER(?) ");
+		       queryParams.add("%"+dnomlp+"%");
+		       
 		       queryBuilder.append(createEqualsClauseRequest("dnupro", dnupro, queryParams));
+		       queryBuilder.append(createEqualsClauseRequest("comptecommunal", compteCommunal, queryParams));
+		       if(details != 2){
+		    	   queryBuilder.append("order by dnomlp, dprnlp ");
+		       }
 		       queryBuilder.append(finalizeQuery());
 	 	       
 		    	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
