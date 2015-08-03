0f4e8fd0d77a23db2fafc01f9c360ddd3ebe713c
pierrejego <pierre.jego@gfi.fr>
2015-08-03 10:43:40 +0200

    Revert "Replace ccoinsee by cgocommune CGOCommune means Code Géographique Officile Commune"
    This reverts commit 64f47115319ea72b56889a0165a26925106ecd56.

--- script/commun/user/groupeAutorisation.sql
+++ script/commun/user/groupeAutorisation.sql
@@ -5,22 +5,21 @@
 CREATE TABLE #schema_cadastrapp.groupe_autorisation
 (
   id serial NOT NULL, -- Id d'indentification de lignes
-  idgroup character varying(30), -- Identifiant du groupe LDAP devant être filtré
-  cgocommune character varying(6), -- Code commune geographique officiel sur 6 caracteres (code departement + code direction (arrondissement) + code commune)
+  idgroup character varying(30), -- Identfiant du groupe LDAP devant être filtré
+  ccoinsee character varying(6), -- Code commune insee sur 6 caractères
   ccodep character varying(3), -- Code département à mettre en relation avec le code commune
   CONSTRAINT groupe_autorisation_pk PRIMARY KEY (id ),
-  CONSTRAINT "groupe_autorisation_UK" UNIQUE (cgocommune , ccodep , idgroup )
+  CONSTRAINT "groupe_autorisation_UK" UNIQUE (ccoinsee , ccodep , idgroup )
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE #schema_cadastrapp.groupe_autorisation
   OWNER TO #user_cadastrapp;
-  
-COMMENT ON TABLE #schema_cadastrapp.groupe_autorisation IS 'Table de correlation entre les groupes LDAP et les droits géographiques';
-
+COMMENT ON TABLE #schema_cadastrapp.groupe_autorisation
+  IS 'Table de correlation entre les groupes LDAP et les droits géographiques';
 COMMENT ON COLUMN #schema_cadastrapp.groupe_autorisation.id IS 'Id d''indentification de lignes';
 COMMENT ON COLUMN #schema_cadastrapp.groupe_autorisation.idgroup IS 'Identfiant du groupe LDAP devant être filtré';
-COMMENT ON COLUMN #schema_cadastrapp.groupe_autorisation.cgocommune IS 'Code commune geographique officiel sur 6 caracteres (code departement + code direction (arrondissement) + code commune)';
+COMMENT ON COLUMN #schema_cadastrapp.groupe_autorisation.ccoinsee IS 'Code commune insee sur 6 caractères';
 COMMENT ON COLUMN #schema_cadastrapp.groupe_autorisation.ccodep IS 'Code département à mettre en relation avec le code commune';
 
