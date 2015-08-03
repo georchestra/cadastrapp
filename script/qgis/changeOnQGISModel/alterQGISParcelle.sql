de42298300b0b65d7e32736ad1344f096b77594b
pierrejego <pierre.jego@gfi.fr>
2015-08-03 11:12:03 +0200

    Add inspireId to Qgis model

--- script/qgis/changeOnQGISModel/alterQGISParcelle.sql
+++ script/qgis/changeOnQGISModel/alterQGISParcelle.sql
@@ -1,1 +1,3 @@
-ALTER TABLE parcelle ADD COLUMN inspireid character varying(16)[];
+--Ajout de l'inspireID comme pracelleId
+ALTER TABLE parcelle ADD COLUMN inspireid character varying(16);
+UDPATE parcelle SET inspireid= cast('FR'||ccodep||ccocom||ccodir||replace(ccopre,' ','0')||ccosec||dnupla as varchar(16));
