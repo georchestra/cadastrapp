436cb08230c283913384c50493e98019ee3a65b2
pierrejego <pierre.jego@gfi.fr>
2015-08-05 11:28:56 +0200

    Add log information to follow execution. user, roles and requestURI is added with MDC in logs.

---
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/providers/PreRequestFilter.java
@@ -0,0 +1,32 @@
+package org.georchestra.cadastrapp.providers;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+/**
+ *  Log all request
+ * @author gfi
+ *
+ */
+public class PreRequestFilter implements ContainerRequestFilter {
+	
+	final Logger logger = LoggerFactory.getLogger(PreRequestFilter.class);
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+			
+		String rolesList = requestContext.getHeaderString("sec-roles");
+		String userName = requestContext.getHeaderString("sec-username");
+		MDC.put("user", userName);
+		MDC.put("roles", rolesList);
+		MDC.put("uri", requestContext.getUriInfo().getPath());	
+		
+		logger.info("Incomming request");
+	}
+}
