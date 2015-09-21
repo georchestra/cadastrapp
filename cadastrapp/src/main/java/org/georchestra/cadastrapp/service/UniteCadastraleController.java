7b07edacce7d94f2be32c86c15e98b978aa62e24
pierrejego <pierre.jego@gfi.fr>
2015-09-21 11:05:15 +0200

    Remove distinct, n proprietaires can have the same comptecommunal

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/UniteCadastraleController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/UniteCadastraleController.java
@@ -125,7 +125,7 @@ public class UniteCadastraleController extends CadController {
 		StringBuilder queryBuilder = new StringBuilder();
 		
 		// CNIL Niveau 1
-		queryBuilder.append("select distinct p.comptecommunal, p.dnupro, p.ddenom, p.dnomlp, p.dprnlp, p.epxnee, p.dnomcp, p.dprncp, p.dlign3, p.dlign4, p.dlign5, p.dlign6, p.dldnss, p.jdatnss, p.ccodro, p.ccodro_lib");
+		queryBuilder.append("select p.comptecommunal, p.dnupro, p.ddenom, p.dnomlp, p.dprnlp, p.epxnee, p.dnomcp, p.dprncp, p.dlign3, p.dlign4, p.dlign5, p.dlign6, p.dldnss, p.jdatnss, p.ccodro, p.ccodro_lib");
 		queryBuilder.append(" from ");
 		queryBuilder.append(databaseSchema);
 		queryBuilder.append(".proprietaire_parcelle propar,");
