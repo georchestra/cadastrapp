c1ea2097317f7befac3b87e40c9c316775ba8b12
pierrejego <pierre.jego@gfi.fr>
2015-09-22 16:25:53 +0200

    New deletion script for materialized views

---
+++ script/arcopole/database/dropALL.sql
@@ -0,0 +1,30 @@
+
+-- Drop all view
+DROP MATERIALIZED VIEW #schema_cadastrapp.commune;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.parcelledetails;
+DROP MATERIALIZED VIEW #schema_cadastrapp.parcelle;
+DROP MATERIALIZED VIEW #schema_cadastrapp.v_parcelle_surfc;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.proprietaire;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.proprietaire_parcelle;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.proprietebatie;
+DROP MATERIALIZED VIEW #schema_cadastrapp.proprietenonbatie;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.descproffessionnel;
+DROP MATERIALIZED VIEW #schema_cadastrapp.descdependance;
+DROP MATERIALIZED VIEW #schema_cadastrapp.deschabitation;
+
+DROP MATERIALIZED VIEW #schema_cadastrapp.section;
+
+-- Drop all tables (not user autorisation tables)
+
+DROP TABLE #schema_cadastrapp.uf_parcelle;
+
+DROP TABLE #schema_cadastrapp.prop_ccodem;
+DROP TABLE #schema_cadastrapp.prop_ccodro;
+DROP TABLE #schema_cadastrapp.prop_ccogrm;
+DROP TABLE #schema_cadastrapp.prop_ccoqua;
+DROP TABLE #schema_cadastrapp.prop_dnatpr;
