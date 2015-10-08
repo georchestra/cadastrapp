cdf2509c8b94d384ea79f596f65638ac6b73f83c
pierrejego <pierre.jego@gfi.fr>
2015-10-08 11:07:13 +0200

    Add new object for imposition element

---
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/model/pdf/Imposition.java
@@ -0,0 +1,129 @@
+package org.georchestra.cadastrapp.model.pdf;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Imposition {
+	
+	private int revenuImposable;
+	
+	private int communeRevenuExonere;
+	
+	private int departementRevenuExonere;
+	
+	private int regionRevenuExonere;
+	
+	private int communeRevenuImposable;
+	
+	private int departementRevenuImposable;
+	
+	private int regionRevenuImposable;
+
+	/**
+	 * @return the revenuImposable
+	 */
+	public int getRevenuImposable() {
+		return revenuImposable;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param revenuImposable the revenuImposable to set
+	 */
+	public void setRevenuImposable(int revenuImposable) {
+		this.revenuImposable = revenuImposable;
+	}
+
+	/**
+	 * @return the communeRevenuExonere
+	 */
+	public int getCommuneRevenuExonere() {
+		return communeRevenuExonere;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param communeRevenuExonere the communeRevenuExonere to set
+	 */
+	public void setCommuneRevenuExonere(int communeRevenuExonere) {
+		this.communeRevenuExonere = communeRevenuExonere;
+	}
+
+	/**
+	 * @return the departementRevenuExonere
+	 */
+	public int getDepartementRevenuExonere() {
+		return departementRevenuExonere;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param departementRevenuExonere the departementRevenuExonere to set
+	 */
+	public void setDepartementRevenuExonere(int departementRevenuExonere) {
+		this.departementRevenuExonere = departementRevenuExonere;
+	}
+
+	/**
+	 * @return the regionRevenuExonere
+	 */
+	public int getRegionRevenuExonere() {
+		return regionRevenuExonere;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param regionRevenuExonere the regionRevenuExonere to set
+	 */
+	public void setRegionRevenuExonere(int regionRevenuExonere) {
+		this.regionRevenuExonere = regionRevenuExonere;
+	}
+
+	/**
+	 * @return the communeRevenuImposable
+	 */
+	public int getCommuneRevenuImposable() {
+		return communeRevenuImposable;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param communeRevenuImposable the communeRevenuImposable to set
+	 */
+	public void setCommuneRevenuImposable(int communeRevenuImposable) {
+		this.communeRevenuImposable = communeRevenuImposable;
+	}
+
+	/**
+	 * @return the departementRevenuImposable
+	 */
+	public int getDepartementRevenuImposable() {
+		return departementRevenuImposable;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param departementRevenuImposable the departementRevenuImposable to set
+	 */
+	public void setDepartementRevenuImposable(int departementRevenuImposable) {
+		this.departementRevenuImposable = departementRevenuImposable;
+	}
+
+	/**
+	 * @return the regionRevenuImposable
+	 */
+	public int getRegionRevenuImposable() {
+		return regionRevenuImposable;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param regionRevenuImposable the regionRevenuImposable to set
+	 */
+	public void setRegionRevenuImposable(int regionRevenuImposable) {
+		this.regionRevenuImposable = regionRevenuImposable;
+	}
+	
+
+}
---
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/model/pdf/ImpositionNonBatie.java
@@ -0,0 +1,41 @@
+package org.georchestra.cadastrapp.model.pdf;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+public class ImpositionNonBatie extends Imposition{
+	
+	int surface;
+	
+	int majorationTerraion;
+
+	/**
+	 * @return the surface
+	 */
+	public int getSurface() {
+		return surface;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param surface the surface to set
+	 */
+	public void setSurface(int surface) {
+		this.surface = surface;
+	}
+
+	/**
+	 * @return the majorationTerraion
+	 */
+	public int getMajorationTerraion() {
+		return majorationTerraion;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param majorationTerraion the majorationTerraion to set
+	 */
+	public void setMajorationTerraion(int majorationTerraion) {
+		this.majorationTerraion = majorationTerraion;
+	}
+
+}
