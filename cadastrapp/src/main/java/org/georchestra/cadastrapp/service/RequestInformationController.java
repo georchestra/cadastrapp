0229571463bdf961d7697b33607cf6d93d0f1442
pierrejego <pierre.jego@gfi.fr>
2015-10-15 18:03:27 +0200

    Add two new services, one to check user and give missing information to be display in windows and explain if there is some limitation. Second service to check if comptecommunal or parcelle information are Ok.

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/RequestInformationController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/RequestInformationController.java
@@ -22,30 +22,22 @@ public class RequestInformationController extends CadController {
 	final static Logger logger = LoggerFactory.getLogger(RequestInformationController.class);
 	
 	@GET
-	@Path("/getRequestValidity")
+	@Path("/checkRequestLimitation")
 	@Produces(MediaType.APPLICATION_JSON)
 	/**
-	 *  /getRequestValidity
+	 *  /checkRequestLimitation
 	 *  
-	 *  return message to indicate if user can make a search or not
+	 *  return user information, and limitation
 	 *  
 	 * 
-	 * @param nom
-	 * @param prenom
-	 * @param adresse
 	 * @param cni
-	 * @param parcelleids
 	 * 
 	 * @return JSON 
 	 * 
 	 * @throws SQLException
 	 */
-	public List<Map<String, Object>> getRequestValidity(
-			@QueryParam("nom") String cgoCommune,
-			@QueryParam("prenom") String dvoilib,
-			@QueryParam("adresse") String adresse,
+	public List<Map<String, Object>> checkRequestLimitation(
 			@QueryParam("cni") String cni,
-			@QueryParam("parcelleids") List<String> parcelleIds,
 			@Context HttpHeaders headers) throws SQLException {
 
 		// Check information in database
@@ -60,6 +52,44 @@ public class RequestInformationController extends CadController {
 
 		return request;
 	}
+	
+	@GET
+	@Path("/checkRequestValidity")
+	@Produces(MediaType.APPLICATION_JSON)
+	/**
+	 *  /checkRequestValidity
+	 *  
+	 *  check if user can make this search (CNIL level and geographic constraints)
+	 *  
+	 * 
+	 * @param cni
+	 * @param comptecommunal
+	 * @param parcelleids
+	 * 
+	 * @return JSON 
+	 * 
+	 * @throws SQLException
+	 */
+	public List<Map<String, Object>> checkRequestValidity(
+			@QueryParam("cni") String cni,
+			@QueryParam("comptecommunal") String compteCommunal,
+			@QueryParam("parcelle") List<String> parcelleIds,
+			@Context HttpHeaders headers) throws SQLException {
+
+		// Check information in database
+		
+		// Denied request
+		// if User has made more than 5 requests in the last week
+		// if User has made more than 10 requests in the last month
+		// if parcelle requested are not in geographic group of connected user
+		
+		List<Map<String, Object>> request = new ArrayList<Map<String, Object>>();
+
+
+		return request;
+	}
+	
+	
 	
 
 }
