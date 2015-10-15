b9460c499bc15ff09e6c4af6ec4f8da21e8c2312
pierrejego <pierre.jego@gfi.fr>
2015-10-15 18:01:57 +0200

    Add jaxb annotation

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/model/request/InformationRequest.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/model/request/InformationRequest.java
@@ -2,6 +2,10 @@ package org.georchestra.cadastrapp.model.request;
 
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class InformationRequest {
 	
 
@@ -9,16 +13,92 @@ public class InformationRequest {
 	private long userId;
 	private Date requestDate;
 	private String parcelleId;
+	private String comptecommunal;
 	
 	public InformationRequest(){}
 	
-	public InformationRequest(int userId, String parcelleId){
+	public InformationRequest(int userId, String parcelleId, String compteCommunal){
 		this.userId = userId;
 		this.parcelleId = parcelleId;
 	}
 	
 	public String toString(){
 		return String.format("Information Request : id=%d, user=%d, parcelle='%s'", requestId, userId, parcelleId);
+	}
+
+	/**
+	 * @return the requestId
+	 */
+	public long getRequestId() {
+		return requestId;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param requestId the requestId to set
+	 */
+	public void setRequestId(long requestId) {
+		this.requestId = requestId;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public long getUserId() {
+		return userId;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the requestDate
+	 */
+	public Date getRequestDate() {
+		return requestDate;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param requestDate the requestDate to set
+	 */
+	public void setRequestDate(Date requestDate) {
+		this.requestDate = requestDate;
+	}
+
+	/**
+	 * @return the parcelleId
+	 */
+	public String getParcelleId() {
+		return parcelleId;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param parcelleId the parcelleId to set
+	 */
+	public void setParcelleId(String parcelleId) {
+		this.parcelleId = parcelleId;
+	}
+
+	/**
+	 * @return the comptecommunal
+	 */
+	public String getComptecommunal() {
+		return comptecommunal;
+	}
+
+	@XmlAttribute
+	/**
+	 * @param comptecommunal the comptecommunal to set
+	 */
+	public void setComptecommunal(String comptecommunal) {
+		this.comptecommunal = comptecommunal;
 	}
 
 }
