00890bb204bcf6043ff55628c097f8766a36bbe6
pierrejego <pierre.jego@gfi.fr>
2015-10-21 17:51:23 +0200

    Add JPA repository for information request

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/repository/RequestRepository.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/repository/RequestRepository.java
@@ -1,15 +1,46 @@
 package org.georchestra.cadastrapp.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.georchestra.cadastrapp.model.request.InformationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+/**
+ * 
+ * @author gfi
+ * 
+ * Repository to get InformationRequest for database
+ *
+ */
 public interface RequestRepository
     extends JpaRepository<InformationRequest, Long>
 {
     
-    List<InformationRequest> findByRequestIdLike(String nom);
+	/**
+	 *  Return all informationRequest where user card number is like given param
+	 *  
+	 * @param cni
+	 * @return List<InformationRequest>
+	 */
+    List<InformationRequest> findByUserCniLike(String cni);
+    
+    /**
+     *  Give the information request corresponding to id given
+     *  
+     * @param requestId - requestId is generated and increment in database
+     * @return InformationRequest
+     */
+    InformationRequest findByRequestId(long requestId);
+    
+    /**
+     *  Count how many information request had been made by a user given is card number since a date
+     *  
+     * @param cni UserRequest cni ( User card number)
+     * @param date
+     * @return number of Information request for this cni after a given date
+     */
+    int countByUserCniAndRequestDateAfter(String cni, Date date);
     
 }
--- cadastrapp/src/main/java/org/georchestra/cadastrapp/repository/UserRequestRepository.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/repository/UserRequestRepository.java
@@ -1,15 +1,25 @@
 package org.georchestra.cadastrapp.repository;
 
-import java.util.List;
 
 import org.georchestra.cadastrapp.model.request.UserRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/**
+ * 
+ * @author gfi
+ *
+ * Repository to get UserRequest for database
+ */
 public interface UserRequestRepository
     extends JpaRepository<UserRequest, Long>
 {
-    
-    List<UserRequest> findByCniLike(String nom);
+	
+    /**
+     *  Get userRequest from database using cni id
+     *  
+     * @param cni
+     * @return UserRequest using given cni
+     */
+    UserRequest findByCni(String cni);
     
 }
