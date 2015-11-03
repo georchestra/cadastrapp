8147a695c8fe758ee52d3f4c0a856fcd0437697f
pierrejego <pierre.jego@gfi.fr>
2015-11-03 16:54:00 +0100

    Add cascade mode to drop sequence and constraint with table

--- script/commun/dropTablesAndViews.sql
+++ script/commun/dropTablesAndViews.sql
@@ -30,9 +30,9 @@ DROP TABLE #schema_cadastrapp.prop_ccoqua;
 DROP TABLE #schema_cadastrapp.prop_dnatpr;
 
 DROP TABLE #schema_cadastrapp.request_information CASCADE;
-DROP TABLE #schema_cadastrapp.request_user_information;
-DROP TABLE #schema_cadastrapp.request_parcelles_information;
-DROP TABLE #schema_cadastrapp.request_coproprietes_information;
-DROP TABLE #schema_cadastrapp.request_comptecommunaux_information;
+DROP TABLE #schema_cadastrapp.request_user_information CASCADE;
+DROP TABLE #schema_cadastrapp.request_parcelles_information CASCADE;
+DROP TABLE #schema_cadastrapp.request_coproprietes_information CASCADE;
+DROP TABLE #schema_cadastrapp.request_comptecommunaux_information CASCADE;
 
 
