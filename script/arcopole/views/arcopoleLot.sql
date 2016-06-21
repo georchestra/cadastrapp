7b4016a3a5733ccf0772622aab774174ea93c6b5
pierrejego <pierre.jego@gfi.fr>
2016-06-21 17:40:04 +0200

    Add lot information for arcopole model

---
+++ script/arcopole/views/arcopoleLot.sql
@@ -0,0 +1,27 @@
+--- Create lot Views based on Arcopole Models
+
+CREATE MATERIALIZED VIEW #schema_cadastrapp.lots AS 
+	SELECT 
+		lot.codlot,
+		lot.id_local, 
+		lot.dnulot, 
+		lot.dnumql, 
+		lot.ddenql
+  	FROM dblink('host=#DBHost_arcopole dbname=#DBName_arcopole user=#DBUser_arcopole password=#DBpasswd_arcopole'::text, 
+  		'select distinct
+  				l.codlot,
+				l.invloc as id_local,
+				ltrim(l.dnulot, ''0'') as dnulot,
+				ltrim(l.dnumql, ''0'') as dnumql,
+				ltrim(l.ddenql, ''0'') as ddenql
+			from #DBSchema_arcopole.dgi_lotloc l 
+			'::text) 
+	lot(
+		codlot character varying(255), 
+		id_local character varying(19), 
+		dnulot character varying(7), 
+		dnumql character varying(7), 
+		ddenql character varying(7));
+
+ALTER TABLE #schema_cadastrapp.lot OWNER TO #user_cadastrapp;
+
