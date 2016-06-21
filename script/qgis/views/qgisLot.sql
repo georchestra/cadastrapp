dc7fa04a68d26c576197bdb73e6cbaff64d3499c
pierrejego <pierre.jego@gfi.fr>
2016-06-21 12:03:12 +0200

    Change ident

--- script/qgis/views/qgisLot.sql
+++ script/qgis/views/qgisLot.sql
@@ -3,20 +3,20 @@
 DROP VIEW cadastrapp_qgis.lot;
 
 CREATE OR REPLACE VIEW #schema_cadastrapp.lot AS 
-	SELECT	lot.local10, 
+	SELECT	lot.id_local, 
 			lot.dnulot, 
 			lot.dnumql, 
 			lot.ddenql
 		FROM dblink('host=#DBHost_qgis dbname=#DBName_qgis user=#DBUser_qgis password=#DBpasswd_qgis'::text, 
  	 		'select distinct
-				l.local10 as local10,
+				l.local10 as id_local,
 				l.dnulot as dnulot,
 				l.dnumql as dnumql,
 				l.ddenql as ddenql
 			from public.lotslocaux l where ddenql not like ''0000000''
 			'::text) 
 		lot(
-			local10 character varying(14), 
+			id_local character varying(14), 
 			dnulot character varying(15), 
 			dnumql character varying(7), 
 			ddenql character varying(7));
