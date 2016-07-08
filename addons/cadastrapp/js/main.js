a5a5be7d4f79e380f4e239da06aa5a91fd43004d
Landry Breuil <breuil@craig.fr>
2016-07-08 11:31:44 +0200

    fix indentation, 4 spaces everywhere

--- addons/cadastrapp/js/main.js
+++ addons/cadastrapp/js/main.js
@@ -42,153 +42,154 @@ GEOR.Addons.Cadastrapp = Ext.extend(GEOR.Addons.Base, {
             }
         });
 
-                GEOR.Addons.Cadastre.menu = new GEOR.Addons.Cadastre.Menu({
-                    map: this.map,
-                    helpUrl: this.options.helpUrl,
-                    popupOptions: {
-                        unpinnable: false,
-                        draggable: true
-                    }
-                });
+        GEOR.Addons.Cadastre.menu = new GEOR.Addons.Cadastre.Menu({
+            map: this.map,
+            helpUrl: this.options.helpUrl,
+            popupOptions: {
+                unpinnable: false,
+                draggable: true
+            }
+        });
 
-                GEOR.Addons.Cadastre.isWMSLayerAdded = false;
+        GEOR.Addons.Cadastre.isWMSLayerAdded = false;
 
-                GEOR.Addons.Cadastre.selection=[];
-                GEOR.Addons.Cadastre.selection.state=[];
-                GEOR.Addons.Cadastre.selection.state.list = this.options.selectedStyle.colorState1;
-                GEOR.Addons.Cadastre.selection.state.selected = this.options.selectedStyle.colorState2;
-                GEOR.Addons.Cadastre.selection.state.details = this.options.selectedStyle.colorState3;
+        GEOR.Addons.Cadastre.selection=[];
+        GEOR.Addons.Cadastre.selection.state=[];
+        GEOR.Addons.Cadastre.selection.state.list = this.options.selectedStyle.colorState1;
+        GEOR.Addons.Cadastre.selection.state.selected = this.options.selectedStyle.colorState2;
+        GEOR.Addons.Cadastre.selection.state.details = this.options.selectedStyle.colorState3;
 
-                GEOR.Addons.Cadastre.relevePropriete=[];
-                GEOR.Addons.Cadastre.relevePropriete.maxProprietaire = 25;
+        GEOR.Addons.Cadastre.relevePropriete=[];
+        GEOR.Addons.Cadastre.relevePropriete.maxProprietaire = 25;
 
-                // Init gobal variables
-                GEOR.Addons.Cadastre.WFSLayer;
-                GEOR.Addons.Cadastre.result=[];
-                GEOR.Addons.Cadastre.result.tabs;
+        // Init gobal variables
+        GEOR.Addons.Cadastre.WFSLayer;
+        GEOR.Addons.Cadastre.result=[];
+        GEOR.Addons.Cadastre.result.tabs;
 
-                // result window for plots and owners
-                GEOR.Addons.Cadastre.result.plot=[];
-                GEOR.Addons.Cadastre.result.plot.window;
-                GEOR.Addons.Cadastre.result.owner=[];
-                GEOR.Addons.Cadastre.result.owner.window;
+        // result window for plots and owners
+        GEOR.Addons.Cadastre.result.plot=[];
+        GEOR.Addons.Cadastre.result.plot.window;
+        GEOR.Addons.Cadastre.result.owner=[];
+        GEOR.Addons.Cadastre.result.owner.window;
 
-                GEOR.Addons.Cadastre.createSelectionControl(this.options.defautStyleParcelle , this.options.selectedStyle);
+        GEOR.Addons.Cadastre.createSelectionControl(this.options.defautStyleParcelle , this.options.selectedStyle);
 
-                this.window = new Ext.Window({
-                    title: OpenLayers.i18n('cadastrapp.cadastre_tools'),
-                    closable: true,
-                    closeAction: "hide",
-                    resizable: true,
-                    border: false,
-                    constrainHeader: true,
-                    boxMaxWidth: 650,
-                    cls: 'cadastrapp',
-                    items: [ {
-                        xtype: 'toolbar',
-                        border: false,
-                        items: GEOR.Addons.Cadastre.menu.items
-                    } ],
-                    listeners: {
-                    	"show": function() {
-                    		GEOR.Addons.Cadastre.addWMSLayer(WMSSetting);
-                    		GEOR.Addons.Cadastre.addPopupOnhover(this.options.popup);
-                    	},
-                        "hide": function() {
+        this.window = new Ext.Window({
+            title: OpenLayers.i18n('cadastrapp.cadastre_tools'),
+            closable: true,
+            closeAction: "hide",
+            resizable: true,
+            border: false,
+            constrainHeader: true,
+            boxMaxWidth: 650,
+            cls: 'cadastrapp',
+            items: [ {
+                xtype: 'toolbar',
+                border: false,
+                items: GEOR.Addons.Cadastre.menu.items
+            } ],
+            listeners: {
+                "show": function() {
+                    GEOR.Addons.Cadastre.addWMSLayer(WMSSetting);
+                    GEOR.Addons.Cadastre.addPopupOnhover(this.options.popup);
+                },
+                "hide": function() {
 
-                        	// deactivate all controls create by cadastrapp addons
-                            // All controls used by cadastrapp are store in the field GEOR.Addons.Cadastre.menu.cadastrappControls
-                        	 Ext.each(GEOR.Addons.Cadastre.menu.cadastrappControls, function(control, index) {
-                                 control.deactivate();
-                             });
-
-                            // Remove WMS Layer
-                            if (GEOR.Addons.Cadastre.isWMSLayerAdded == true && GEOR.Addons.Cadastre.WMSLayer != null) {
-
-                            	 var layersWMS = [];
-                                 layersWMS.push(GEOR.Addons.Cadastre.WMSLayer);
-                                 var reader = new GeoExt.data.LayerReader();
-                                 var layerData = reader.readRecords(layersWMS);
-
-                                 GeoExt.MapPanel.guess().layers.remove(layerData.records[0]);
-
-                               // this.map.removeLayer(GEOR.Addons.Cadastre.WMSLayer);
-                                GEOR.Addons.Cadastre.WMSLayer.destroy();
-                                GEOR.Addons.Cadastre.WMSLayer = null;
-                                GEOR.Addons.Cadastre.addPopupOnhover = null ; //
-                            }
-
-                            // Remove all windows
-                            if(GEOR.Addons.Cadastre.popup){
-                                GEOR.Addons.Cadastre.popup.close();
-                                GEOR.Addons.Cadastre.popup=null
-                            }
-                            if (GEOR.Addons.Cadastre.result.plot.window){
-                                GEOR.Addons.Cadastre.result.plot.window.close();
-                                GEOR.Addons.Cadastre.result.plot.window=null;
-                            }
-
-                            if (GEOR.Addons.Cadastre.result.owner.window){
-                                GEOR.Addons.Cadastre.result.owner.window.close();
-                                GEOR.Addons.Cadastre.result.owner.window=null;
-                            }
-
-                            if ( GEOR.Addons.Cadastre.proprietaireWindow){
-                                GEOR.Addons.Cadastre.proprietaireWindow.close();
-                                GEOR.Addons.Cadastre.proprietaireWindow = null;
-                            }
-
-                            if (GEOR.Addons.Cadastre.rechercheParcelleWindow){
-                                GEOR.Addons.Cadastre.rechercheParcelleWindow.close();
-                                GEOR.Addons.Cadastre.rechercheParcelleWindow = null;
-                            }
-
-                            if( GEOR.Addons.Cadastre.printBordereauParcellaireWindow){
-                                GEOR.Addons.Cadastre.printBordereauParcellaireWindow.close();
-                                GEOR.Addons.Cadastre.printBordereauParcellaireWindow=null;
-                            }
-
-                            if( GEOR.Addons.Cadastre.request.informationsWindow){
-                                GEOR.Addons.Cadastre.request.informationsWindow.close();
-                                GEOR.Addons.Cadastre.request.informationsWindow=null;
-                            }
-
-                            if(GEOR.Addons.Cadastre.coProprieteWindow){
-                                GEOR.Addons.Cadastre.coProprieteWindow.close();
-                                GEOR.Addons.Cadastre.coProprieteWindow=null;
-                            }
-
-                        },
-                        scope: this
-                    }
-                });
-
-                if (this.target) {
-                    // create a button to be inserted in toolbar:
-                    this.components = this.target.insertButton(this.position, {
-                        xtype: 'button',
-                        tooltip: this.getTooltip(record),
-                        iconCls: "addon-cadastrapp",
-                        handler: this._onCheckchange,
-                        scope: this
+                    // deactivate all controls create by cadastrapp addons
+                    // All controls used by cadastrapp are store in the field GEOR.Addons.Cadastre.menu.cadastrappControls
+                    Ext.each(GEOR.Addons.Cadastre.menu.cadastrappControls, function(control, index) {
+                        control.deactivate();
                     });
-                    this.target.doLayout();
-                    // create a menu item for the "tools" menu:
-                    this.item = new Ext.menu.CheckItem({
-                        text: this.getText(record),
-                        qtip: this.getQtip(record),
-                        iconCls: "addon-cadastrapp",
-                        checked: false,
-                        listeners: {
-                            "checkchange": this._onCheckchange,
-                            scope: this
-                        }
-                    });
+
+                    // Remove WMS Layer
+                    if (GEOR.Addons.Cadastre.isWMSLayerAdded == true && GEOR.Addons.Cadastre.WMSLayer != null) {
+
+                        var layersWMS = [];
+                        layersWMS.push(GEOR.Addons.Cadastre.WMSLayer);
+                        var reader = new GeoExt.data.LayerReader();
+                        var layerData = reader.readRecords(layersWMS);
+
+                        GeoExt.MapPanel.guess().layers.remove(layerData.records[0]);
+
+                        // this.map.removeLayer(GEOR.Addons.Cadastre.WMSLayer);
+                        GEOR.Addons.Cadastre.WMSLayer.destroy();
+                        GEOR.Addons.Cadastre.WMSLayer = null;
+                        GEOR.Addons.Cadastre.addPopupOnhover = null ; //
+                    }
+
+                    // Remove all windows
+                    if(GEOR.Addons.Cadastre.popup){
+                        GEOR.Addons.Cadastre.popup.close();
+                        GEOR.Addons.Cadastre.popup=null
+                    }
+                    if (GEOR.Addons.Cadastre.result.plot.window){
+                        GEOR.Addons.Cadastre.result.plot.window.close();
+                        GEOR.Addons.Cadastre.result.plot.window=null;
+                    }
+
+                    if (GEOR.Addons.Cadastre.result.owner.window){
+                        GEOR.Addons.Cadastre.result.owner.window.close();
+                        GEOR.Addons.Cadastre.result.owner.window=null;
+                    }
+
+                    if ( GEOR.Addons.Cadastre.proprietaireWindow){
+                        GEOR.Addons.Cadastre.proprietaireWindow.close();
+                        GEOR.Addons.Cadastre.proprietaireWindow = null;
+                    }
+
+                    if (GEOR.Addons.Cadastre.rechercheParcelleWindow){
+                        GEOR.Addons.Cadastre.rechercheParcelleWindow.close();
+                        GEOR.Addons.Cadastre.rechercheParcelleWindow = null;
+                    }
+
+                    if( GEOR.Addons.Cadastre.printBordereauParcellaireWindow){
+                        GEOR.Addons.Cadastre.printBordereauParcellaireWindow.close();
+                        GEOR.Addons.Cadastre.printBordereauParcellaireWindow=null;
+                    }
+
+                    if( GEOR.Addons.Cadastre.request.informationsWindow){
+                        GEOR.Addons.Cadastre.request.informationsWindow.close();
+                        GEOR.Addons.Cadastre.request.informationsWindow=null;
+                    }
+
+                    if(GEOR.Addons.Cadastre.coProprieteWindow){
+                        GEOR.Addons.Cadastre.coProprieteWindow.close();
+                        GEOR.Addons.Cadastre.coProprieteWindow=null;
+                    }
+
+                },
+                scope: this
+            }
+        });
+
+        if (this.target) {
+            // create a button to be inserted in toolbar:
+            this.components = this.target.insertButton(this.position, {
+                xtype: 'button',
+                tooltip: this.getTooltip(record),
+                iconCls: "addon-cadastrapp",
+                handler: this._onCheckchange,
+                scope: this
+            });
+            this.target.doLayout();
+            // create a menu item for the "tools" menu:
+            this.item = new Ext.menu.CheckItem({
+                text: this.getText(record),
+                qtip: this.getQtip(record),
+                iconCls: "addon-cadastrapp",
+                checked: false,
+                listeners: {
+                    "checkchange": this._onCheckchange,
+                    scope: this
                 }
-		if (this.options.openToolbarOnLoad) {
-			this.window.show();
-			this.window.alignTo(Ext.get(this.map.div), "t-t", [ 0, 5 ], true);
-		}
+            });
+        }
+
+        if (this.options.openToolbarOnLoad) {
+            this.window.show();
+            this.window.alignTo(Ext.get(this.map.div), "t-t", [ 0, 5 ], true);
+        }
     },
 
 
@@ -196,12 +197,12 @@ GEOR.Addons.Cadastrapp = Ext.extend(GEOR.Addons.Base, {
      * Method: _onCheckchange Callback on checkbox state changed
      */
     _onCheckchange: function(item, checked) {
-    	if (checked && !this.window.isVisible()) {
-    		this.window.show();
-    		this.window.alignTo(Ext.get(this.map.div), "t-t", [ 0, 5 ], true);
-    	} else {
-    		this.window.hide();
-    	}
+        if (checked && !this.window.isVisible()) {
+            this.window.show();
+            this.window.alignTo(Ext.get(this.map.div), "t-t", [ 0, 5 ], true);
+        } else {
+            this.window.hide();
+        }
     },
 
     /**
@@ -255,7 +256,7 @@ GEOR.Addons.Cadastrapp = Ext.extend(GEOR.Addons.Base, {
             GEOR.Addons.Cadastre.coProprieteWindow=null;
         }
 
-     // Add test in case add-ons did not load properly
+        // Add test in case add-ons did not load properly
         if(GEOR.Addons.Cadastre.menu){
             GEOR.Addons.Cadastre.menu.destroy();
         }
