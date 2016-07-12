ff1220fec52ccabc6b38e63bdeef8c3bf8eb4207
Landry Breuil <breuil@craig.fr>
2016-07-12 14:35:14 +0200

    Use jsbuild to build a minified cadastrapp.js (#181)
    Note that this relies on georchestra/georchestra#1420

---
+++ cadastrapp/jsbuild/jsbuild.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+buildpath="$(cd $(dirname $0); pwd)"
+srcpath="${buildpath}/../../addons/cadastrapp/js/"
+releasepath="${srcpath}/build"
+venv="${buildpath}/env"
+
+#
+# Command path definitions
+#
+python="/usr/bin/python"
+mkdir="/bin/mkdir"
+rm="/bin/rm"
+sh="/bin/sh"
+cp="/bin/cp"
+
+
+
+#
+# build
+#
+if [ -d ${releasepath} ]; then
+    ${rm} -rf ${releasepath}
+fi
+
+${mkdir} -p ${releasepath}
+
+(cd ${buildpath};
+ ${venv}/bin/jsbuild -h > /dev/null
+ if  [ ! -d ${venv} ] || [ $? -eq 0 ]; then
+     echo "creating virtual env and installing jstools..."
+     rm -rf ${venv}
+     virtualenv  --no-site-packages ${venv}
+     ${venv}/bin/pip install jstools
+     echo "done."
+ fi;
+
+ echo "running jsbuild for main app..."
+ ${venv}/bin/jsbuild -o "${releasepath}" main.cfg
+ echo "done."
+)
+
+if [ ! -e ${releasepath}/cadastrapp.js ]; then
+    echo "\033[01;31m[NOK]\033[00m jsbuild failure"
+    exit 1
+fi;
+
+exit 0
