322e2de6f8022e637935509aac81e944756dc36e
pierrejego <pierre.jego@gfi.fr>
2016-10-18 14:36:15 +0200

    Remove duplicate attribute

--- script/qgis/views/qgisProprieteBatie.sql
+++ script/qgis/views/qgisProprieteBatie.sql
@@ -32,8 +32,6 @@ CREATE MATERIALIZED VIEW #schema_cadastrapp.proprietebatie AS
 		proprietebatie.mvltieomx,
 		proprietebatie.bateom,
 		proprietebatie.jannat,
-		proprietebatie.cconlc,
-		proprietebatie.dcapec,
 		proprietebatie.revcad,
 		proprietebatie.rcexba2,
 		proprietebatie.rcbaia_tse,
