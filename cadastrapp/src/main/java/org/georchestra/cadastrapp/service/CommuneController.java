25d8219d637ce2357903c9fd4c8a158b3b00dacc
pierrejego <pierre.jego@gfi.fr>
2017-04-11 17:45:20 +0200

    Compare value from given input to libcom_maj without removing accent
    This is link to #296

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CommuneController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CommuneController.java
@@ -65,9 +65,8 @@ public class CommuneController extends CadController{
 	    	 // Check if libcom is not null
 	    	if(libCom != null && !libCom.isEmpty() && libCom.length()>=minNbCharForSearch){
 		    	
-	    		// Remove all accent from url
-	    		String newLibCom = StringUtils.stripAccents(libCom);	    		    	 
-	    		isWhereAdded = createRightLikeClauseRequest(isWhereAdded, queryBuilder, "libcom", newLibCom.toUpperCase(), queryParams);      
+	    		// Remove all accent from url	    		    	 
+	    		isWhereAdded = createRightLikeClauseRequest(isWhereAdded, queryBuilder, "libcom_maj", libCom.toUpperCase(), queryParams);      
 	    	}
 	    	else{
 	    		logger.info("LibCom has not enough characters to launch research with libCom");
