86c6e9f84e10502fa5d49681b771e9fa2ce187c4
François Van Der Biest <francois.vanderbiest@gmail.com>
2017-09-25 13:25:51 +0200

    Set jstools package version to fix build issues

--- cadastrapp/jsbuild/jsbuild.sh
+++ cadastrapp/jsbuild/jsbuild.sh
@@ -30,8 +30,8 @@ ${mkdir} -p ${releasepath}
  if  [ ! -d ${venv} ] || [ $? -eq 0 ]; then
      echo "creating virtual env and installing jstools..."
      rm -rf ${venv}
-     virtualenv  --no-site-packages -p python3 ${venv}
-     ${venv}/bin/pip install jstools
+     virtualenv  --no-site-packages ${venv}
+     ${venv}/bin/pip install jstools==0.6
      echo "done."
  fi;
 
