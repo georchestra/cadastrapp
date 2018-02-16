0a9ced3da00a694965ef7ed4d4fae57407ff80a5
pierre <pierre@georchestra>
2018-02-16 22:46:17 +0100

    Remove control on map
    link #243

--- addons/cadastrapp/js/ficheUniteFonciere.js
+++ addons/cadastrapp/js/ficheUniteFonciere.js
@@ -1,24 +1,4 @@
 var printMap;
-/**
- * Create date as 11072017154030
- */
-function formatDate() {
-    var d = new Date(),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear(),
-        hour = d.getHours(),
-        minutes = d.getMinutes(),
-        seconds = d.getSeconds();
-    if (month.length < 2) {
-        month = '0' + month
-    };
-    if (day.length < 2) {
-        day = '0' + day
-    };
-    var date = day + month + year + hour + minutes + seconds;
-    return date;
-}
 
 /**
  * Init code after body is loaded
@@ -26,8 +6,7 @@ function formatDate() {
 function init() {
     var clones,
         baseLayerPosition,
-        openerLayers = [],
-        openerMap = new Object();
+        openerMap;
     
     //activate JQuery UI actions for this element     
     $('#printmap').draggable().resizable();    
@@ -37,16 +16,16 @@ function init() {
 
         // get original map and layers from mapfishapp viewer
         openerMap = window.opener.GeoExt.MapPanel.guess().map; 
-        openerLayers = openerMap.layers;
         
         // clones layer and get informations to call legend
         clones = [];
         baseLayerPosition = "";        
-        openerLayers.forEach(function(layer, index) {            
+        openerMap.layers.forEach(function(layer, index) {            
             if (layer.clone) {                        
                 if(layer.attribution){ // to not display source on map
                     delete layer["attribution"];
                 }
+               
                 clones.push(layer.clone());
                 // get and set base layer from original viewer
                 // This name is specific to mapfishapp.
@@ -58,19 +37,17 @@ function init() {
         // new map    
         printMap = new OpenLayers.Map("printmap", {            
             units: "m",
-            scales: openerMap.scales
+            scales: openerMap.scales,
+            controls: [
+                new OpenLayers.Control.ScaleLine()
+                ] 
         });
-        // create cotrols and add to map
-        var scaleCtr = new OpenLayers.Control.ScaleLine();        
-        var navCtr = new OpenLayers.Control.Navigation();
-        printMap.setOptions({
-            controls: [scaleCtr, navCtr]
-        });
+        
         // add clones layers
         printMap.addLayers(clones);
         // set base layer
         if (baseLayerPosition !== "") {
-            printMap.setBaseLayer(printMap.layers[0]);
+            printMap.setBaseLayer(printMap.layers[baseLayerPosition]);
         }
         // zoom to original extend
         printMap.setCenter(new OpenLayers.LonLat(openerMap.center.lon, openerMap.center.lat), openerMap.getZoom());
@@ -79,27 +56,6 @@ function init() {
         var parcelleId = window.opener.GEOR.Addons.Cadastre.UF.parcelleId;
         getUFInformation(parcelleId);
     }
-}
-
-/**
- * Download file or image to computer without server
- * @param {String} filename - Text input by user from prompt window
- * @param {String} text - Text that will be insert in file if user download json
- * @param {String} dataURL - Data URI containing a representation of the image in 96 dpi resolution if user download page in PNG
- */
-function download(filename, text, dataURL) {
-    // create temporary element to trigger download
-    var link = document.createElement('a');
-    if (text) {
-        link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    } else {
-        link.href = dataURL;
-    }
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    link = null;
 }
 
 /**
@@ -118,41 +74,6 @@ function handlerVisibility(el, bool) {
         });
     }
 }
-/**
- * Create canvas and download canvas as image
- * @param {e} - not use
- */
-function getCanvas(e) {
-    // create function to trigger download
-    function saveAsFile() {
-        var dateStr = formatDate();
-        var fileName = prompt("Entrez un nom de fichier:", "Fiche_unite_fonciere" + dateStr + ".png");
-        if (fileName !== null && fileName !== "") {
-            if (fileName.indexOf(".png") < 0) {
-                fileName = fileName + ".png";
-            }
-            // change style rules to do not display resizable handler
-            var l = [printMap.div];
-            l.forEach(function(el) {
-                handlerVisibility(el, false);
-            });
-            // fire create canvas by html2canvas lib - return only mainPage as canvas            
-            html2canvas(document.getElementById("mainPage"), {
-                proxy: window.opener.GEOR.Addons.Cadastre.cadastrappWebappUrl+"/canvaProxy/",
-                onrendered: function(canvas) {
-                    var dataURL = canvas.toDataURL("image/png");
-                    download(fileName, false, dataURL);
-                    // change style rules to do display resizable handler
-                    l.forEach(function(el) {
-                        handlerVisibility(el, true);
-                    });
-                }
-            });
-        }
-    }
-    // fire download
-    saveAsFile();
-}
 
 /**
  * 
@@ -164,19 +85,27 @@ function getUFInformation(parcelleId){
     // get global information
     $.getJSON( window.opener.GEOR.Addons.Cadastre.cadastrappWebappUrl+"getInfoUniteFonciere?parcelle=" +  encodeURIComponent(parcelleId), function(data) {
         
-        // get owner name
-        $.getJSON( window.opener.GEOR.Addons.Cadastre.cadastrappWebappUrl+"getProprietaire?details=2&comptecommunal=" +  encodeURIComponent(data.comptecommunal), function(prop) {
-            var propName = propName=prop[0].app_nom_usage;
-           
-            document.getElementById('informationgenerale').innerHTML =
-            "<div class=\"datauf\"><span class=\"dataufLabel\">Propriétaire : </span>"+propName+"</div>"+
-            "<div class=\"datauf\"><span class=\"dataufLabel\"></span>"+data.comptecommunal+"</div>"+
-            "<div class=\"datauf\"><span class=\"dataufLabel\">Surface DGFIP de l'UF : </span>"+data.dcntpa_sum.toLocaleString()+" m²</div>"+
-            "<div class=\"datauf\"><span class=\"dataufLabel\">Surface calculée : </span>"+data.sigcal_sum.toLocaleString()+" m²</div>"+
-            "<div class=\"datauf\"><span class=\"dataufLabel\">Surface bâtie calculée : </span>"+data.sigcalb_sum.toLocaleString()+" m²</div>";
-        });
-
-        getParcellesInformation(data.uf);
+        // Only if we get a response
+        if(data){
+            // get owner name
+            $.getJSON( window.opener.GEOR.Addons.Cadastre.cadastrappWebappUrl+"getProprietaire?details=2&comptecommunal=" +  encodeURIComponent(data.comptecommunal), function(prop) {
+               
+                var propName = "";
+                prop.forEach(function(element){
+                    propName = propName +"<div>"+element.app_nom_usage+"</div>";
+                });
+                          
+                document.getElementById('informationgenerale').innerHTML =
+                "<div class=\"proprieteaire\"><div>Propriétaire(s) : </div>"+propName+
+                "<div>"+data.comptecommunal+"</div></div>"+
+                "<div class=\"datauf\"><span class=\"dataufLabel\">Surface DGFIP de l'UF : </span>"+((data.dcntpa_sum === null) ? 0 : data.dcntpa_sum.toLocaleString())+" m²</div>"+
+                "<div class=\"datauf\"><span class=\"dataufLabel\">Surface calculée : </span>"+((data.sigcal_sum === null) ? 0 : data.sigcal_sum.toLocaleString())+" m²</div>"+
+                "<div class=\"datauf\"><span class=\"dataufLabel\">Surface bâtie calculée : </span>"+((data.sigcalb_sum === null) ? 0 : data.sigcalb_sum.toLocaleString())+" m²</div>";
+            });
+    
+            getParcellesInformation(data.uf);
+        }
+        
       }); 
 }
 
@@ -193,7 +122,7 @@ function getParcellesInformation(ufId){
         var sommeSurf=0;
         
         data.forEach(function(element){
-            parcelles=parcelles+ "<div class=\"data\"><span class=\"dataLabel\">"+element.parcelle+"</span>"+element.surfc.toLocaleString()+" m²</div>";
+            parcelles=parcelles+ "<div class=\"data\"><span class=\"dataLabel\">"+element.parcelle+"</span>"+((element.surfc === null) ? 0 : element.surfc.toLocaleString())+" m²</div>";
             sommeSurf=sommeSurf+element.surfc;
         });
          
