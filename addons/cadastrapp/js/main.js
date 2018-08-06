676d9680a0ec1d088f1cbf1c8b55ff3096881c55
Landry Breuil <breuil@craig.fr>
2018-08-06 13:32:45 +0200

    Add the wfs layer to the map when showing the toolbar (#404)

--- addons/cadastrapp/js/main.js
+++ addons/cadastrapp/js/main.js
@@ -116,6 +116,7 @@ GEOR.Addons.Cadastrapp = Ext.extend(GEOR.Addons.Base, {
                     this.item.setChecked(true, true);
                     GEOR.Addons.Cadastre.visible = true;
                     GEOR.Addons.Cadastre.addWMSLayer(WMSSetting);
+                    this.map.addLayer(GEOR.Addons.Cadastre.WFSLayer);
                     GEOR.Addons.Cadastre.addPopupOnhover(this.options.popup);
                     var maxX =  window.innerWidth;
                     this.window.setPagePosition(maxX * 0.3 ,0);
