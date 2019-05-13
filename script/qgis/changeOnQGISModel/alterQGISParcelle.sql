ad234024a685b77bb2edca245c7987d1bd7a9d4c
MaelREBOUX <m.reboux@rennesmetropole.fr>
2019-05-13 14:44:40 +0200

    Delete alterQGISParcelle.sql

--- script/qgis/changeOnQGISModel/alterQGISParcelle.sql
+++
@@ -1,3 +0,0 @@
---Ajout de l'inspireID comme pracelleId
-ALTER TABLE parcelle ADD COLUMN inspireid character varying(17);
-UPDATE parcelle SET inspireid='FR'||ccodep||ccocom||ccodir||replace(ccopre,' ','0')||ccosec||dnupla;
