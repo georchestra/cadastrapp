446ad59be85bf14ecd104fbe07405d3ebe9996d4
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-04-15 11:17:31 +0200

    proprietenonbatie

--- database/sql/vues/ProprieteNonBatie.sql
+++ database/sql/vues/ProprieteNonBatie.sql
@@ -1,5 +1,7 @@
 -- Create view proprietenonbatie based on Qgis Models
 
+-- DROP MATERIALIZED VIEW #schema_cadastrapp.proprietenonbatie ;
+
 CREATE MATERIALIZED VIEW #schema_cadastrapp.proprietenonbatie AS 
 	SELECT 
 		proprietenonbatie.id_local, -- Identifiant du local
@@ -35,20 +37,21 @@ CREATE MATERIALIZED VIEW #schema_cadastrapp.proprietenonbatie AS
 		proprietenonbatie.bisufad_com, -- Base d'imposition de la subdivision fiscale - commune
 		proprietenonbatie.bisufad_dep, -- Base d'imposition de la subdivision fiscale - département
 		proprietenonbatie.bisufad_gp -- Base d'imposition de la subdivision fiscale - groupement communal
-	FROM dblink('host=#DBHost_qgis port=#DBPort_qgis dbname=#DBName_qgis user=#DBUser_qgis password=#DBpasswd_qgis'::text, 
-		'select 
+	FROM 
+	(
+		SELECT 
 			suf.suf as id_local,
-			COALESCE(p.jdatat, '''') as jdatat,
+			COALESCE(p.jdatat, '') as jdatat,
 			p.comptecommunal,
 			p.ccodep || p.ccodir ||	p.ccocom as cgocommune,
 			ltrim(p.ccopre) as ccopre,
 			ltrim(p.ccosec) as ccosec,
-			ltrim(p.dnupla, ''0'') as dnupla,
+			ltrim(p.dnupla, '0') as dnupla,
 			p.parcelle,
 			p.ccovoi,
 			p.voie,
 			p.ccoriv,
-			ltrim(p.dnvoiri, ''0'') as dnvoiri,
+			ltrim(p.dnvoiri, '0') as dnvoiri,
 			p.dindic,
 			v.natvoi,
 			p.cconvo,
@@ -70,45 +73,14 @@ CREATE MATERIALIZED VIEW #schema_cadastrapp.proprietenonbatie AS
 			suftax.c1bisufad as bisufad_com,
 			suftax.c2bisufad as bisufad_dep,
 			suftax.c4bisufad as bisufad_gp
-		from #DBSchema_qgis.parcelle p
-			left join #DBSchema_qgis.voie v on v.voie=p.voie
-			left join #DBSchema_qgis.suf on suf.comptecommunal=p.comptecommunal and p.parcelle=suf.parcelle
-			left join #DBSchema_qgis.suftaxation as suftax on suftax.suf=suf.suf
-			left join #DBSchema_qgis.cgrnum as gnc on gnc.cgrnum = suf.cgrnum
-			left join #DBSchema_qgis.dsgrpf as sga on sga.dsgrpf = suf.dsgrpf
-			left join #DBSchema_qgis.cnatsp as cncs on cncs.cnatsp = suf.cnatsp'::text)
-	proprietenonbatie(
-		id_local character varying(21),  
-		jdatat character varying(10),  
-		comptecommunal character varying(15), 
- 		cgocommune character varying(6), 
- 		ccopre character varying(3),
- 		ccosec character varying(2),  
- 		dnupla character varying(4), 
- 		parcelle character varying(19),
- 		ccovoi character varying(5), 
- 		voie character varying(19), 
- 		ccoriv character varying(4), 
- 		dnvoiri character varying(4), 
- 		dindic character varying(1), 
- 		natvoi character varying(4), 
- 		cconvo character varying(4), 
- 		dvoilib character varying(26), 
- 		dparpi character varying(4), 
- 		gparnf character varying(1),
- 		ccostn character varying(1), 
- 		ccosub character varying(2), 
- 		cgrnum character varying,
- 		dsgrpf character varying, 
- 		dclssf character varying(2), 
- 		cnatsp character varying,
- 		dcntsf integer, 
- 		drcsuba numeric(10,2), 
- 		pdl character varying(22), 
- 		dnulot character varying(7), 
- 		dreflf character varying(5),
- 		majposa numeric(10,2),
-		bisufad_com numeric(10,2),
-		bisufad_dep numeric(10,2),
-		bisufad_gp numeric(10,2));
+		FROM #DBSchema_qgis.parcelle p
+			LEFT JOIN #DBSchema_qgis.voie v on v.voie=p.voie
+			LEFT JOIN #DBSchema_qgis.suf on suf.comptecommunal=p.comptecommunal and p.parcelle=suf.parcelle
+			LEFT JOIN #DBSchema_qgis.suftaxation as suftax on suftax.suf=suf.suf
+			LEFT JOIN #DBSchema_qgis.cgrnum as gnc on gnc.cgrnum = suf.cgrnum
+			LEFT JOIN #DBSchema_qgis.dsgrpf as sga on sga.dsgrpf = suf.dsgrpf
+			LEFT JOIN #DBSchema_qgis.cnatsp as cncs on cncs.cnatsp = suf.cnatsp
+	) AS proprietenonbatie ;
+
 ALTER TABLE #schema_cadastrapp.proprietenonbatie OWNER TO #user_cadastrapp;
+
