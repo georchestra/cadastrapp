b2d68981948730a7c2e882fc94d007907a70763e
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-07-06 16:02:00 +0200

    suppression de l'ancien script de chargement des données

--- script/qgis/comments/qgisCoProprieteParcelleComments.sql
+++
@@ -1,3 +0,0 @@
--- Create view making link beetween parcelle, owners and lots based on Qgis Models
-
-COMMENT ON MATERIALIZED VIEW #schema_cadastrapp.co_propriete_parcelle IS 'Copropriété';
--- script/qgis/comments/qgisSectionComments.sql
+++
@@ -1,5 +0,0 @@
--- Create view section based on Qgis Models
-COMMENT ON MATERIALIZED VIEW #schema_cadastrapp.section IS 'Section';
-COMMENT ON COLUMN #schema_cadastrapp.section.ccosec IS 'Section cadastrale';
-COMMENT ON COLUMN #schema_cadastrapp.section.ccopre IS 'Préfixe de section ou quartier';
-
