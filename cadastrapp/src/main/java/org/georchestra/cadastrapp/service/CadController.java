d268764123212a477e051bf803bfe2ce24cec4f4
Pierre Jégo <pierre.jego@jdev.fr>
2021-09-17 11:11:28 +0200

    Update configuration to have swagger-ui swagger-api index.jsp working at the sametime. Add /services/* mapping in web.xml and not in RequestMapping

--- cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CadController.java
+++ cadastrapp/src/main/java/org/georchestra/cadastrapp/service/CadController.java
@@ -14,14 +14,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 
  * @author pierre jego
  *
  */
-@RequestMapping("/services/*")
 public class CadController {
 	
 	
