6dc2264f0d39fca1d83a58dde12c889ef455ae95
MaelREBOUX <m.reboux@rennesmetropole.fr>
2022-09-29 15:33:36 +0200

    doc : environnement de build

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ language = 'fr'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', 'venv']
 
 
 # -- Options for HTML output -------------------------------------------------
